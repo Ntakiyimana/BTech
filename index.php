c756d6516961a5256e135f50635273480e507a57
mugabe <wmugabe@yahoo.com>
2024-03-18 08:32:49 +0200

    well added files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dockersample</title>
+</head>
+<body>
+    <?php
+    echo"hello word sample!!!!!!!!!!!";
+    
+    ?>
+</body>
+</html>
